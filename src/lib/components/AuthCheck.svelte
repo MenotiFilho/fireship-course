7d0e58330b7f1835175047da04eeda0ab0663509
Menoti Filho <menotimfilho@gmail.com>
2023-07-23 00:44:50 -0300

    file upload done

--- src/lib/components/AuthCheck.svelte
+++ src/lib/components/AuthCheck.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-import {user} from "$lib/firebase"
+	import { user } from '$lib/firebase';
 </script>
 
 {#if $user}
-<slot />
+	<slot />
 {:else}
-<p class="text-error">
-  You must be signed in to view this page.
-  <a class="btn btn-primary" href="/login">Sign in</a>
-</p>
+	<p class="text-error flex flex-col items-center justify-center gap-5">
+		You must be signed in to view this page.
+		<a class="btn btn-primary w-fit" href="/login">Sign in</a>
+	</p>
 {/if}
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,8 +1,8 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, onAuthStateChanged, type User } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { doc, getFirestore, onSnapshot } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
-import { writable } from 'svelte/store';
+import { writable, type Readable, derived } from 'svelte/store';
 
 const firebaseConfig = {
 	apiKey: 'AIzaSyD7hHDtCzhfw7KRXYHCWG_1UQ5n-m9cesA',
@@ -41,3 +41,45 @@ function userStore() {
 }
 
 export const user = userStore();
+
+/**
+ * @param  {string} path document path or reference
+ * @returns a store with realtime updates on document data
+ */
+export function docStore<T>(path: string) {
+	let unsubscribe: () => void;
+
+	const docRef = doc(db, path);
+
+	const { subscribe } = writable<T | null>(null, (set) => {
+		unsubscribe = onSnapshot(docRef, (snapshot) => {
+			set((snapshot.data() as T) ?? null);
+		});
+
+		return () => unsubscribe();
+	});
+
+	return {
+		subscribe,
+		ref: docRef,
+		id: docRef.id,
+	};
+}
+
+interface UserData {
+	username: string;
+	bio: string;
+	photoURL: string;
+	links: any[];
+}
+
+export const userData: Readable<UserData | null> = derived(
+	user,
+	($user, set) => {
+		if ($user) {
+			return docStore<UserData>(`users/${$user.uid}`).subscribe(set);
+		} else {
+			set(null);
+		}
+	}
+);
